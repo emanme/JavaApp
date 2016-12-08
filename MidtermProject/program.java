c36c056d9f321a483f5767a2dd5afacb20893a7b
EMannue <emannuelsaligue@hotmail.com>
2016-12-08 16:41:26 +0800

    remove error in Display function

--- MidtermProject/program.java
+++ MidtermProject/program.java
@@ -2,10 +2,13 @@ public class program{
 public static void main (String [] ar){
 System.out.print ("Hello world");
 program p =  new  program();
+p.Display;
+}
 public void Display(){
 
 }
-}
+public void Display(int x){
 
+}
 
 }
